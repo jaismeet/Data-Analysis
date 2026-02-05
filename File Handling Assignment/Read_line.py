0a8da78d9d4506a633d6745ba61e48b322deaaba
jaismeet <kaurjaismeet294@gmail.com>
2026-02-05 18:45:41 +0530

    Delete File Handling Assignment directory

--- File Handling Assignment/Read_line.py
+++
@@ -1,8 +0,0 @@
-with open("ABC.txt",'w') as file:
-    file.write("    hello world  ")
-
-def read_file_line_by_line(file_path):
-    with open(file_path, 'r') as file:
-        for line in file:
-            print(line.strip())
-read_file_line_by_line('ABC.txt')
