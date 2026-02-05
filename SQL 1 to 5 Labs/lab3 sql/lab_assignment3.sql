72bf2c38bcebcb768e47bbd8a034b8a939428aba
jaismeet <kaurjaismeet294@gmail.com>
2026-02-05 18:47:01 +0530

    Delete SQL 1 to 5 Labs directory

--- SQL 1 to 5 Labs/lab3 sql/lab_assignment3.sql
+++
@@ -1,6 +0,0 @@
--- Task: Retrieve products that are either in the 'Electronics' category or have a price less than 70,000.
-use ecommerce;
-SELECT * FROM Product
-WHERE category = 'Electronics' OR price < 70000;
-
-
